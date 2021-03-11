67b79b19f9e951d049997f7c51c17472045cef85
FernandoGiganto <fernandogigantorodriguez@gmail.com>
2021-03-11 16:54:16 +0100

    Definición de roles

--- sdi-entrega1-2021-906/src/main/java/com/uniovi/services/InsertSampleDataService.java
+++ sdi-entrega1-2021-906/src/main/java/com/uniovi/services/InsertSampleDataService.java
@@ -17,20 +17,29 @@ public class InsertSampleDataService {
 	@Autowired
 	private UsersService usersService;
 	
+	@Autowired
+	private RolesService rolesService;
+	
 	@PostConstruct
 	public void init() {
 		User user1= new User("pedrodiaz@gmail.com", "Pedro", "Díaz");
 		user1.setPassword("123456");
+		user1.setRole(rolesService.getRoles()[0]);
 		User user2= new User("lucasnuñez@gmail.com", "Lucas", "Núñez");
 		user2.setPassword("123456");
+		user2.setRole(rolesService.getRoles()[0]);
 		User user3= new User("mariarodriguez@gmail.com", "María", "Rodríguez");
 		user3.setPassword("123456");
+		user3.setRole(rolesService.getRoles()[0]);
 		User user4= new User("martaalmonte@gmail.com", "Marta", "Almonte");
 		user4.setPassword("123456");
+		user4.setRole(rolesService.getRoles()[0]);
 		User user5= new User("pelayovaldes@gmail.com", "Pelayo", "Valdes");
 		user5.setPassword("123456");
+		user5.setRole(rolesService.getRoles()[0]);
 		User user6= new User("admin@gmail.com", "Fernando", "Giganto");
 		user6.setPassword("admin");
+		user1.setRole(rolesService.getRoles()[1]);
 		
 		Set user1Offers = new HashSet<Offer>() {
 			{
