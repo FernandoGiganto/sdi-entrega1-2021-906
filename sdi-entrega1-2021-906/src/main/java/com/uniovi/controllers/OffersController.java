e76962bd3430b9624628cbcb9a74fb7963fab460
FernandoGiganto <fernandogigantorodriguez@gmail.com>
2021-03-11 09:26:48 +0100

    Opcion eliminar oferta de "Mis ofertas"

--- sdi-entrega1-2021-906/src/main/java/com/uniovi/controllers/OffersController.java
+++ sdi-entrega1-2021-906/src/main/java/com/uniovi/controllers/OffersController.java
@@ -5,8 +5,12 @@ import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
+import com.uniovi.entities.Offer;
 import com.uniovi.entities.User;
 import com.uniovi.services.OffersService;
 import com.uniovi.services.UsersService;
@@ -30,4 +34,23 @@ public class OffersController {
 		return"offer/list";
 	}
 
+	
+	@RequestMapping(value = "/offer/add", method = RequestMethod.GET)
+	public String setMark(Model model) {
+		model.addAttribute("usersList", usersService.getUsers());
+		return "offer/add";
+	}
+
+	@RequestMapping(value = "/offer/add", method = RequestMethod.POST)
+	public String setMark(@ModelAttribute Offer offer) {
+
+		offersService.addOffer(offer);
+		return "redirect:/offer/list";
+	}
+	
+	@RequestMapping("/offer/delete/{id}")
+	public String deleteOffer(@PathVariable Long id) {
+		offersService.deleteOffer(id);
+		return "redirect:/offer/list";
+	}
 }
